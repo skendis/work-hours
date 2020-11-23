c393580bd9e5b6d7c6e45340ac9d9f325448a415
skendis <sergei.kendis@gmail.com>
2020-11-23 22:02:01 +0200

    refactor + add date

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,11 +8,6 @@ import {AbstractControl, FormBuilder} from '@angular/forms';
   encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
-
-  time;
-  totalHours = 0;
-  totalMinutes = 0;
-  base = 0;
   workHours = this.fb.array([]);
   form = this.fb.group({
     workHours: this.workHours
@@ -22,48 +17,70 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.addTime();
+    this.init();
+  }
+
+  init(): void {
+    const date = new Date();
+    const days = this.DaysInMonth(date.getFullYear(), date.getMonth());
+    for (let i = 2; i <= days; i++) {
+      this.addFormGroup(new Date(date.getFullYear(), date.getMonth(), i));
+    }
   }
 
 
-  diff(start, end): string {
+  /**
+   * @description calculate time diff of start and end
+   * @param start hours:minutes 00:00
+   * @param end hours:minutes 00:00
+   */
+  diff(start, end): number {
     start = start.split(':');
     end = end.split(':');
     const startDate = new Date(0, 0, 0, start[0], start[1], 0);
     const endDate = new Date(0, 0, 0, end[0], end[1], 0);
-    let diff = endDate.getTime() - startDate.getTime();
-    let hours = Math.floor(diff / 1000 / 60 / 60);
-    diff -= hours * 1000 * 60 * 60;
-    const minutes = Math.floor(diff / 1000 / 60);
-
-    if (hours < 0) {
-      hours = hours + 24;
-    }
-
-    this.totalHours += hours;
-    this.totalMinutes += minutes;
-    if (this.totalMinutes >= 60) {
-      this.totalHours += 1;
-      this.totalMinutes -= 60;
-    }
-    return (hours <= 9 ? '0' : '') + hours + ':' + (minutes <= 9 ? '0' : '') + minutes;
+    return endDate.getTime() - startDate.getTime();
   }
-
 
   calcTime(formGroup: AbstractControl): void {
     const startTime = formGroup.get('startTime').value;
     const endTime = formGroup.get('endTime').value;
     this.diff(startTime, endTime);
-
-    this.addTime();
   }
 
-  addTime(): void {
-    const newTime = this.fb.group({
+  /**
+   * @description returns days in month
+   * @param year - selected year
+   * @param month - selected month
+   */
+  DaysInMonth(year, month): number {
+    return new Date(year, month, 0).getDate();
+  }
+
+
+  addFormGroup(date: Date): void {
+    const group = this.fb.group({
       startTime: [''],
-      endTime: ['']
+      endTime: [''],
+      shiftDate: [{value: date.toISOString().substring(0, 10), disabled: true}]
     });
-    this.workHours.push(newTime);
+    this.workHours.push(group);
   }
+
+
+  // let hours = Math.floor(diff / 1000 / 60 / 60);
+  // diff -= hours * 1000 * 60 * 60;
+  // const minutes = Math.floor(diff / 1000 / 60);
+  // hours = hours < 0 ? hours + 24 : hours;
+
+
+  // this.totalHours += hours;
+  // this.totalMinutes += minutes;
+  // if (this.totalMinutes >= 60) {
+  //   this.totalHours += 1;
+  //   this.totalMinutes -= 60;
+  // }
+  // return (hours <= 9 ? '0' : '') + hours + ':' + (minutes <= 9 ? '0' : '') + minutes;
+
 
 }
